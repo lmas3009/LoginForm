23568c7c978f48600a5bc877b25f70d337779776
Lmas Lmas <30lmas09@gmail.com>
2020-03-26 21:57:05 +0530

    edit

--- loginform.js
+++ loginform.js
@@ -1,6 +1,11 @@
 function LoginF(options){
-    let info = documnet.querySelectorAll("div");
-    info.style.color='red';
+    let fname = documnet.querySelector(".fullname");
+    let email = documnet.querySelector(".email");
+    let password = documnet.querySelector(".password");
+    let uname = documnet.querySelector(".username");
+    let instaram = documnet.querySelector(".insta");
+    let facebook = documnet.querySelector(".face");
+    uname.style.color='red';
 }
 
 module.exports.LoginF = LoginF;
