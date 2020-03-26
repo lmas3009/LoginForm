eafbb832eb10a1d80788986a51679a8bbb75252a
Lmas Lmas <30lmas09@gmail.com>
2020-03-26 21:06:06 +0530

    edit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LoginForm
+
+This is used to create a Login page with all css included 
+
+# installation
+
+`npm i loginform `
+
---
+++ loginform.js
@@ -0,0 +1,6 @@
+function LoginForm(options){
+    let info = documnet.querySelectorAll("div");
+    info.style.color='red';
+}
+
+module.exports.LoginForm = LoginForm;
